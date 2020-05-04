407839d2fe129be6240e1bcceee4cc0a1f038883
Lhouceine OUHAMZA <41924314+ouhamzalhss@users.noreply.github.com>
2020-05-04 12:35:48 +0000

    update

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Salam 3likom les amis");
+        System.out.println("Salam 3likom les amis!!");
     }
 }
