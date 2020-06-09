af3b40efa28778b8ec249ba3f6f65e6141ac98c1
Lhouceine OUHAMZA <41924314+ouhamzalhss@users.noreply.github.com>
2020-06-09 15:18:43 +0100

    update

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ import java.util.stream.Stream;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Salam 3likom mes amis!!");
-        Stream.of("Bouganfou","Tagguine","El bakal").forEach(s->{
+        Stream.of("Bouganfou","Tagguine","El bakal","Tarik").forEach(s->{
              System.out.println(s);
         });
     }
